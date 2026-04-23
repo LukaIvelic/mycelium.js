92c9194e213905d0ae705e7513eb9ab28ddd41d5
Luka Ivelić <ivelicluka@gmail.com>
2026-04-23 10:25:57 +0200

    fix: ran prettier

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,7 +4,7 @@ export const LOG_ENDPOINT = 'www.myceliums.dev/api/logs';
 export const BODY_MAX_BYTES = BodyBytes.FIVE_KB;
 
 export const AssertReadyMessages = {
-    API_KEY: 'Mycelium client requires an api key before initialize().',
-    SERVICE_KEY: 'Mycelium client requires service.key before initialize().',
-    SERVICE_ORIGIN: 'Mycelium client requires service.origin before initialize().',
-}
+  API_KEY: 'Mycelium client requires an api key before initialize().',
+  SERVICE_KEY: 'Mycelium client requires service.key before initialize().',
+  SERVICE_ORIGIN: 'Mycelium client requires service.origin before initialize().',
+};
